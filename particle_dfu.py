012012e2754768f9466be281e1deb03293d190c0
Kean Wong <siliconkiwi@gmail.com>
2019-01-30 22:49:37 -1000

    Discovers an attached Particle device on the serial interface, then sends it the correct configuration to cause the Particle board to switch to DFU mode.

---
+++ particle_dfu.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+# Do this: "chmod +x particle_dfu.py"
+# Then add the directory into $PATH
+
+import subprocess
+import sys
+
+
+def main():
+    modem_port = get_modem_port()
+    print("Particle serial interface found at :" + modem_port)
+    dfu_command = "stty -f " + modem_port + " 14400"
+    returned_value = subprocess.run(dfu_command, shell=True)
+    if returned_value.returncode == 0:
+        print("Particle board at " + modem_port + " has been put in DFU mode.")
+
+def get_modem_port():
+    returned_value = subprocess.run("ls /dev/cu.usb*", stdout=subprocess.PIPE, shell=True)
+    if returned_value.returncode == 1:
+        print("No Particle board connected!")
+        sys.exit(1)
+
+    modem_port = str.strip(returned_value.stdout.decode("utf-8"))
+    return modem_port
+
+
+if __name__ == '__main__':
+    main()
